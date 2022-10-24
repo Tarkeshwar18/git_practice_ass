9be09f646c2c724b12e5b1e1b7c6ed94d89d7301
Tarkeshwar18 <tarkeshwaryadav18jan1998@gmail.com>
2022-10-24 13:29:02 +0530

    code_correction

---
+++ login5.js
@@ -0,0 +1,14 @@
+//Prime Identify
+
+let x=0
+let num=13;
+for(let i=2; i<=num; i++){
+    if(num%i==0){
+        console.log("N0");
+        x+=1;
+        break;
+    }
+}
+for(x==0){
+    console.log("Yes");
+}
